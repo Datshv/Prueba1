c858ecce3c0ef656c4a30c0e26d5e0c4b239f64b
Datshv <38699856+Datshv@users.noreply.github.com>
2018-07-17 13:30:37 -0500

    comit 1 de neuvo

---
+++ Proyecto2/src/test/java/tests2/Test1.java
@@ -0,0 +1,19 @@
+package tests2;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(true);
+	}
+}
